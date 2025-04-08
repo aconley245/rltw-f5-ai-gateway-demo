9b96aa70c90f254e9c373de7db694ec7f58ab3dc
Andy Conley <a.conley@f5.com>
2025-04-08 11:30:59 -0600

    changed ollama to banking-assistant

--- service/ollama/run_ollama.sh
+++ service/ollama/run_ollama.sh
@@ -5,5 +5,5 @@ ollama serve &  # Start Ollama in the background
 
 echo "Ollama is ready, creating the model..."
 
-ollama create retail-assistant -f model_files/Modelfile
-ollama run retail-assistant
+ollama create banking-assistant -f model_files/Modelfile
+ollama run banking-assistant
